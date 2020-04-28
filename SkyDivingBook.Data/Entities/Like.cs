a83990468d208f9efacbf8509488048689907e94
abramepithey <abramepithey@gmail.com>
2020-04-28 18:59:07 -0400

    filled out Like entity

--- SkyDivingBook.Data/Entities/Like.cs
+++ SkyDivingBook.Data/Entities/Like.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,13 @@ namespace SkyDivingBook.Data.Entities
     {
         [Key]
         public int LikeId { get; set; }
-        public Post LikedPost { get; set; }
-        public User Liker { get; set; }
+
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
