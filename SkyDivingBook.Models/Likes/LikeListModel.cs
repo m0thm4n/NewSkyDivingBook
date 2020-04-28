4680dc205db459335e41586eb825932197e74bbc
abramepithey <abramepithey@gmail.com>
2020-04-28 19:08:35 -0400

    added Like models

--- SkyDivingBook.Models/Likes/LikeListModel.cs
+++ SkyDivingBook.Models/Likes/LikeListModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SkyDivingBook.Models.Likes
 {
-    class LikeDeleteModel
+    public class LikeListModel
     {
+        public int PostId { get; set; }
+        public string Name { get; set; }
     }
 }
